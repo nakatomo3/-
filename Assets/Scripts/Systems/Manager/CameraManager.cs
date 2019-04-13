173c817663d72efb7e7e47308197731e68bada10
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-04-13 10:20:10 +0900

    カメラの微移動を追加、 プレイヤーがジャンプ時に少し低速移動になるように

--- Assets/Scripts/Systems/Manager/CameraManager.cs
+++ Assets/Scripts/Systems/Manager/CameraManager.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
 
-	public CameraManager instance;
+	public static CameraManager instance;
+
+	private Transform playerTransform;
+	private Transform thisTransform;
+
+	[SerializeField]
+	private float SideRange = 0;
+	private const float SIDE_MAX_RAMGE = 3;
+
+	float posX;
+	float posY;
 
 	private void Awake() {
 		instance = this;
+		thisTransform = transform;
+		playerTransform = Player.instance.gameObject.transform;
 	}
 
 
 	// Start is called before the first frame update
 	void Start() {
-
+		thisTransform.Rotate(0, -0.8f, 0);
+		thisTransform.position = playerTransform.position + new Vector3(0, 4.8f, -20f);
 	}
 
 	// Update is called once per frame
 	void Update() {
 
+		thisTransform.position = new Vector3(playerTransform.position.x + SideRange, posY, thisTransform.position.z);
+	}
+
+	public void MoveRight() {
+		SideRange += Time.deltaTime * Player.instance.moveSpeed;
+
+		if(SideRange >= SIDE_MAX_RAMGE) {
+			SideRange = SIDE_MAX_RAMGE;
+		}
+	}
+
+	public void MoveLeft() {
+		SideRange -= Time.deltaTime * Player.instance.moveSpeed;
+
+		if (SideRange <= -SIDE_MAX_RAMGE) {
+			SideRange = -SIDE_MAX_RAMGE;
+		}
+
+	}
+
+	private void FixedUpdate() {
+		posY = Mathf.Lerp(thisTransform.position.y, playerTransform.position.y+4f, 0.1f);
 	}
 }
