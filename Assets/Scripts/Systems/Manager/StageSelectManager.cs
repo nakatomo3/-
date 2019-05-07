b72fc971ae2298d6173de60f78306517cbf5a879
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-05-07 15:05:14 +0900

    ステージセレクトでコレクトパーツの状況を見れるように コレクトパーツを全て収集していると隠しステージへの道が開けるように

--- Assets/Scripts/Systems/Manager/StageSelectManager.cs
+++ Assets/Scripts/Systems/Manager/StageSelectManager.cs
@@ -7,7 +7,12 @@ public class StageSelectManager : MonoBehaviour {
 
 	public static StageSelectManager instance;
 
-	public int selectingStageNum=0;
+	public int selectingStageNum = 0;
+
+	public GameObject hiddenStagePipe;
+
+	public GameObject collectPartsObject;
+	public GameObject[,] collectParts = new GameObject[5,2];
 
 	private void Awake() {
 		instance = this;
@@ -15,47 +20,18 @@ public class StageSelectManager : MonoBehaviour {
 
 	// Start is called before the first frame update
 	void Start() {
-		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 2; j++) {
+				collectParts[i, j] = Instantiate(collectPartsObject, new Vector3(21 + j * 3, 5.6f + 9.4f * i, -1.8f), Quaternion.identity) as GameObject;
+			}
+		}
 	}
 
 	// Update is called once per frame
 	void Update() {
+		hiddenStagePipe.SetActive(!CheckCollectpartsAll());
 
 	}
-        //#if UNITY_EDITOR
-        //		if (Input.anyKey) {
-        //			PlayerPrefs.SetInt("stage", selectingStageNum);
-        //			SceneManager.LoadScene("Game");
-        //		}
-        //#endif
-
-
-//        if (Input.GetMouseButtonDown(0)) {
-//            Ray ray = new Ray();
-//            RaycastHit hit = new RaycastHit();
-//            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-//            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity)) {
-
-//                //ゲーム開始
-//#if UNITY_EDITOR
-//                if (hit.collider.gameObject.tag=="Game") {
-//                    selectingStageNum = 1;
-//                    PlayerPrefs.SetInt("stage", selectingStageNum);
-//                    SceneManager.LoadScene("Game");
-//                }
-//#endif
-
-//                //タイトルへ戻る
-//                if (hit.collider.gameObject.tag=="Title") {
-//                    SceneManager.LoadScene("Title");
-
-
-//                }
-//            }
-//        }
-
-    //}
 
 	public void ChangeStage(int n) {
 		//selectingStageNumのステージに移動
@@ -64,5 +40,25 @@ public class StageSelectManager : MonoBehaviour {
 		SceneManager.LoadScene("Game");
 	}
 
+	private bool CheckCollectpartsAll() {
+		bool isNotHaveCollectParts = true;
+		for(int i = 0; i < 5; i++) {
+			if(PlayerPrefs.GetInt(i.ToString() + 1) == 0) {
+				isNotHaveCollectParts = false;
+				collectParts[i, 0].SetActive(false);
+			} else {
+				collectParts[i, 0].SetActive(true);
+			}
+			if (PlayerPrefs.GetInt(i.ToString() + 2) == 0) {
+				isNotHaveCollectParts = false;
+				collectParts[i, 1].SetActive(false);
+
+			} else {
+				collectParts[i, 0].SetActive(true);
+
+			}
+		}
+		return isNotHaveCollectParts;
+	}
 
 }
