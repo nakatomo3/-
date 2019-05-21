cb583d7aafa6ab8f427fe5d8323386a7fe8456dc
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-05-21 15:07:50 +0900

    全てのステージをクリアした際のアニメーションを追加

--- Assets/Scripts/Systems/Manager/StageSelectManager.cs
+++ Assets/Scripts/Systems/Manager/StageSelectManager.cs
@@ -11,6 +11,7 @@ public class StageSelectManager : MonoBehaviour {
 
 	public GameObject hiddenStagePipe;
 	public GameObject appearObjectParent;
+	public GameObject gradation;
 
 	public GameObject collectPartsObject;
 	private GameObject[,] collectParts = new GameObject[5,2];
@@ -25,6 +26,15 @@ public class StageSelectManager : MonoBehaviour {
 	private const float ANIMATION_DOWN_MAX = 3;
 	public GameObject Gradation;
 
+	private bool isEndAnimation = false;
+	private float endAnimationTimer = 0;
+	private const float END_ANIMATION_MAX = 4;
+	public GameObject endSign;
+	private List<Transform> endSignGears = new List<Transform>();
+	private bool isCrackerPoped = false;
+	public GameObject crackerPaperA;
+	public GameObject crackerPaperB;
+
 	private void Awake() {
 		instance = this;
 	}
@@ -36,6 +46,7 @@ public class StageSelectManager : MonoBehaviour {
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 2; j++) {
 				collectParts[i, j] = Instantiate(collectPartsObject, new Vector3(20 + j * 2, 5.6f + 9.4f * i, -1.8f), Quaternion.identity,collectpartsParent.transform) as GameObject;
+				collectParts[i, j].SetActive(false);
 			}
 		}
 		for(int i = 0; i < 5; i++) {
@@ -43,7 +54,7 @@ public class StageSelectManager : MonoBehaviour {
 		}
 
 		if(CheckCollectpartsAll() == true) {
-			if (PlayerPrefs.GetInt("freeAnimation") == 0) {
+			if (PlayerPrefs.GetInt("freeAnimation",0) == 0) {
 				isAnimation = true;
 				CameraManager.instance.isFreeAnimation = true;
 				PlayerPrefs.SetInt("freeAnimation", 1);
@@ -51,11 +62,20 @@ public class StageSelectManager : MonoBehaviour {
 				doors[5] = Instantiate(Door, new Vector3(27, -7, 1.8f), Quaternion.identity, doorParent);
 				hiddenStagePipe.SetActive(false);
 				appearObjectParent.SetActive(true);
+				gradation.transform.position += Vector3.down * 15;
 			}
 		}
 		PlayerPrefs.SetInt("isTutorial", 0);
 
-
+		for(int i = 1; i < 6; i++) {
+			if(PlayerPrefs.GetInt("Clear"+i.ToString(),0) == 0) {
+				//return;
+			}
+		}
+		for(int i = 0; i < endSign.transform.GetChild(3).childCount; i++) {
+			endSignGears.Add(endSign.transform.GetChild(3).GetChild(i));
+		}
+		isEndAnimation = true;
 	}
 
 	// Update is called once per frame
@@ -66,7 +86,7 @@ public class StageSelectManager : MonoBehaviour {
 			}
 		}
 
-		if (isAnimation) {
+		if (isAnimation == true && isEndAnimation == false) {
 			animationTimer += Time.deltaTime;
 			if (animationTimer < ANIMATION_DOWN_MAX) {
 				Gradation.transform.position += Vector3.down * Time.deltaTime * 5;
@@ -78,6 +98,29 @@ public class StageSelectManager : MonoBehaviour {
 			if (animationTimer > ANIMATION_DOWN_MAX + 1f){
 				isAnimation = false;
 				CameraManager.instance.isFreeAnimation = false;
+			}
+		}
+
+		if(isEndAnimation == true && isAnimation == false) {
+			endAnimationTimer += Time.deltaTime;
+			if(endAnimationTimer < END_ANIMATION_MAX) {
+				endSign.transform.position += Vector3.down * 5 * Time.deltaTime;
+				for(int i = 0; i < endSignGears.Count; i++) {
+					endSignGears[i].Rotate(0, 0, 100 * Time.deltaTime*((-1 * i % 2) + 0.5f) * 2);
+				}
+			} else if(endAnimationTimer < END_ANIMATION_MAX + 0.05f){
+				endSign.transform.position += Vector3.down * 50 * Time.deltaTime;
+			}else if(endAnimationTimer < END_ANIMATION_MAX + 5) {
+				if (isCrackerPoped == false) {
+					for(int i =  0; i < 200; i++) {
+						Instantiate(crackerPaperA,CameraManager.instance.transform);
+					}
+					isCrackerPoped = true;
+				}
+			}else if(endAnimationTimer < END_ANIMATION_MAX * 2 + 5) {
+				endSign.transform.position -= Vector3.down * 5 * Time.deltaTime;
+			} else {
+				isEndAnimation = false;
 			}
 		}
 
@@ -97,13 +140,13 @@ public class StageSelectManager : MonoBehaviour {
 	private bool CheckCollectpartsAll() {
 		bool isNotHaveCollectParts = true;
 		for(int i = 1; i < 5+1; i++) {
-			if(PlayerPrefs.GetInt(i.ToString() + "1") == 0) {
+			if(PlayerPrefs.GetInt(i.ToString() + "1",0) == 0) {
 				isNotHaveCollectParts = false;
 				collectParts[i-1, 0].SetActive(false);
 			} else {
 				collectParts[i-1, 0].SetActive(true);
 			}
-			if (PlayerPrefs.GetInt(i.ToString() + "2") == 0) {
+			if (PlayerPrefs.GetInt(i.ToString() + "2",0) == 0) {
 				isNotHaveCollectParts = false;
 				collectParts[i-1, 1].SetActive(false);
 
---
+++ Assets/Scripts/Systems/Others/CrackerPaper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackerPaper : MonoBehaviour {
+
+	public bool isA;
+	private Transform thisTransform;
+	private float rotateSpeed = 0;
+	private const float ROTATE_SPEED_MAX = 10;
+	private float fallSpeed = 0;
+	private const float FALL_SPEED_MAX = 5;
+
+	private Renderer renderer;
+
+	// Start is called before the first frame update
+	void Start() {
+		renderer = GetComponent<Renderer>();
+		renderer.material.color = new Color(Random.value, Random.value, Random.value, 1);
+		rotateSpeed = (Random.value - 0.5f) * 2 * ROTATE_SPEED_MAX;
+
+		fallSpeed = Random.Range(0, FALL_SPEED_MAX);
+
+		thisTransform = transform;
+
+		transform.localPosition = new Vector3((Random.value - 0.5f) * 2 * 20, Random.value * 15 + 10, 23);
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		thisTransform.Rotate(rotateSpeed+1,rotateSpeed /3,0);
+		thisTransform.position += Vector3.down * (fallSpeed + 5) * Time.deltaTime;
+
+		if(thisTransform.position.y < -15) {
+			Destroy(gameObject);
+		}
+	}
+}
