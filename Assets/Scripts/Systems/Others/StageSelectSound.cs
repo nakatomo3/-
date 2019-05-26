4da2b8fba3e0d0b6c2190cc35d5a1ff348dd902d
sugimotokento <keyunutoma@gmail.com>
2019-05-26 21:47:42 +0900

    stageSelectに音を付けた

---
+++ Assets/Scripts/Systems/Others/StageSelectSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectSound : MonoBehaviour
+{
+    private AudioSource sound01;
+    private AudioSource steam;
+    private float steamPlaySoundCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        sound01 = audioSources[0];
+        steam = audioSources[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (steamPlaySoundCounter >= 10) {
+            steam.PlayOneShot(steam.clip);
+            steamPlaySoundCounter = 0;
+
+        }
+        steamPlaySoundCounter += Time.deltaTime;
+    }
+}
